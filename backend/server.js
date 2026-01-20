f66690bbdde9a5091a56ff0bde2e81680ca2bce5
Asametmete <sametmete3436@gmail.com>
2026-01-20 01:54:43 +0100

    some error fixed

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     'http://localhost:3000',
-    'https://your-netlify-site.netlify.app'
+    'https://weather-project-backend.onrender.com'
   ],
   credentials: true
 };
